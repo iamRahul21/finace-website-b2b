875707257b6bd13d675280b4ac2ee393903bec2f
iamRahul21 <rahuldeka072@gmail.com>
2024-11-30 22:15:05 +0530

    seonc page 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Phone from './Components/PhoneMockUp/Phone'
+import './app.css'
+import Features from './Components/Features/Features'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Phone />
+      <div id="page1">
+        <Navbar />
+        <Hero />
+        <Phone />
+      </div>
+      <div id="page2">
+        <Features />
+      </div>
     </div>
   )
 }
---
+++ src/Components/Features/Features.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Features.scss'
+
+const Features = () => {
+    return (
+        <div className="features-section">
+            <h1 className='heading'>Services</h1>
+            <ul>
+                <li>Employee Mobile App</li>
+                <li>Employer Dashboard</li>
+                <li>Efficient Back-end</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Features
