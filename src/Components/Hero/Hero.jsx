19118a647a0f2f49ec11677e645caebb32a73ad7
iamRahul21 <rahuldeka072@gmail.com>
2024-12-03 17:31:00 +0530

    assets restructured

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
             </div>
             <div className="hero-right">
                 <img
-                    src="\yes.svg"
+                    src="/yes.svg"
                     alt="Finace App"
                     className='phone-mockup'
                     style={{
