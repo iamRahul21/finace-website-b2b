166641d2c590972e1f1a7ba8eac035b1c8e8b309
iamRahul21 <rahuldeka072@gmail.com>
2024-12-08 21:53:08 +0530

    hero page text updates

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,13 +5,18 @@ const Hero = () => {
     return (
         <div className="hero-section">
             <div className="hero-left">
-                <div className='hero-para-1'>Ace your Finances with <div className="underline">Early Salary Access!</div></div>
-                {/* <div className="hero-para-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam itaque quam iusto quasi quas, quo adipisci pariatur in doloribus incidunt!</div> */}
+                <div className='hero-para-1'>
+                    <span class="line-one">Ace your Finances</span><br />
+                    <span class="line-two">
+                        with <span class="color">Finace</span>
+                    </span>
+                </div>
+                <div className="hero-para-2">Take control of your financial well-being! Start with Early Salary Access today as we bring you a complete suite of financial services tailored for your growth.</div>
                 <div className='button'>
                     <div className="get-started">Get Started</div>
                     <div className="book-a-demo">Book a demo</div>
                 </div>
-
+                <div className="hero-para-3">Discover how you can access your earned salary before month-end</div>
             </div>
             <div className="hero-right">
                 <img
@@ -19,7 +24,7 @@ const Hero = () => {
                     alt="Finace App"
                     className='phone-mockup'
                     style={{
-                        height: "70vh", width: "auto"
+                        height: "80vh", width: "auto"
                     }}
                 />
             </div>
