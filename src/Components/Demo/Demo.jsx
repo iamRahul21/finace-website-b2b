9917c79da8af0f40840df7f4b954cd4b13aa5081
iamRahul21 <rahuldeka072@gmail.com>
2024-12-14 14:00:39 +0530

    request demo page new layout

--- src/Components/Demo/Demo.jsx
+++ src/Components/Demo/Demo.jsx
@@ -4,12 +4,21 @@ import './Demo.scss'
 const Demo = () => {
     return (
         <div className="demo-section">
-            <h1 className="demo-h1">Request Demo</h1>
-            <h2 className="demo-h2">Are you ready to experience the power of Finace?</h2>
-            <p className="demo-p">
-                Take action today and seize the opportunity to revolutionize your workforce's financial well-being by scheduling a demo slot with us. Our team of experts will guide you through a personalized demo, tailored to your specific business needs.
-            </p>
-            <a href="/" className="demo-btn">Book a Demo</a>
+            <div className="demo-txt">
+                <h2>Request Demo</h2>
+                <h2>Request Demo</h2>
+            </div>
+            <div className="content">
+                <div className="demo-left">
+                    <h2 className="demo-h2">Are you ready to experience the power of Finace?</h2>
+                    <p className="demo-p">
+                        Take action today and seize the opportunity to revolutionize your workforce's financial well-being by scheduling a demo slot with us. Our team of experts will guide you through a personalized demo, tailored to your specific business needs.
+                    </p>
+                </div>
+                <div className="demo-right">
+                    <a href="/" className="demo-btn">Book a Demo</a>
+                </div>
+            </div>
         </div>
     )
 }
