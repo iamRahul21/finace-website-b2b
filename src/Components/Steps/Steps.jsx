e7a237c957e4fbacb3d5f56db3739adcfd092762
iamRahul21 <rahuldeka072@gmail.com>
2024-12-16 14:33:27 +0530

    steps new layout

--- src/Components/Steps/Steps.jsx
+++ src/Components/Steps/Steps.jsx
@@ -36,47 +36,44 @@ const Steps = () => {
 
     return (
         <section id="timeline">
-            <h1>How Finace Works</h1>
+            <h1 className='steps-head'>How Finace Works</h1>
             <p className="leader">
                 With Finace, managing your finances has never been easier. Our platform works seamlessly in four simple steps:
             </p>
             <div className="demo-card-wrapper">
-                <div className="demo-card demo-card--step1">
+                <div className="demo-card">
                     <div className="head">
                         <div className="number-box">
-                            <span>01</span>
+                            01
                         </div>
                         <h2>Employee requests earned pay on Finace.</h2>
                     </div>
                 </div>
 
-                <div className="demo-card demo-card--step2">
+                <div className="demo-card">
                     <div className="head">
                         <div className="number-box">
-                            <span>02</span>
+                            02
                         </div>
                         <h2>Finace credits the earned salary instantly to the employee's account.</h2>
                     </div>
                 </div>
 
-                <div className="demo-card demo-card--step3">
+                <div className="demo-card">
                     <div className="head">
                         <div className="number-box">
-                            <span>03</span>
+                            03
                         </div>
                         <h2>Finace reconciles the earned salary with your company's payroll.</h2>
                     </div>
                 </div>
 
-                <div className="demo-card demo-card--step4">
+                <div className="demo-card">
                     <div className="head">
                         <div className="number-box">
-                            <span>04</span>
+                            04
                         </div>
-                        <h2>
-                            Employers gain real-time insights into the financial analytics of the workforce, including earned wage
-                            access and employee turnover rates.
-                        </h2>
+                        <h2>Employers gain real-time insights into the financial analytics of the workforce, including earned wage access and employee turnover rates.</h2>
                     </div>
                 </div>
             </div>
