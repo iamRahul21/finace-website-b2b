16336aeaae0e52542433ffb0362320e2837c3aaf
iamRahul21 <rahuldeka072@gmail.com>
2025-01-19 22:27:37 +0530

    demo small page animation

--- src/Components/Demo/Demo.jsx
+++ src/Components/Demo/Demo.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import './Demo.scss';
 
 const Demo = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.4,
+  });
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  const buttonVariants = {
+    hidden: { scale: 0.8, opacity: 0 },
+    visible: { scale: 1, opacity: 1, transition: { duration: 0.5 } },
+  };
+
   const scrollToRequestDemo = () => {
     const requestDemoSection = document.getElementById('page8');
     if (requestDemoSection) {
@@ -10,26 +27,39 @@ const Demo = () => {
 
       window.scrollTo({
         top: offsetPosition,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
     }
   };
 
   return (
-    <div className="demo-section">
-      {/* <div className="demo-txt">
-        <h2>Request Demo</h2>
-        <h2>Request Demo</h2>
-      </div> */}
-
+    <motion.div
+      className="demo-section"
+      ref={ref}
+      initial="hidden"
+      animate={inView ? 'visible' : 'hidden'}
+      variants={containerVariants}
+      transition={{ duration: 0.8, ease: 'easeOut' }}
+    >
       <div className="content">
-        <div className="demo-left">
+        <motion.div
+          className="demo-left"
+          initial="hidden"
+          animate={inView ? 'visible' : 'hidden'}
+          variants={containerVariants}
+          transition={{ duration: 0.8, ease: 'easeOut' }}
+        >
           <h2 className="demo-h2">Are you ready to experience the power of Finace?</h2>
           <p className="demo-p">
             Take action today and seize the opportunity to revolutionize your workforce's financial well-being by scheduling a demo slot with us. Our team of experts will guide you through a personalized demo, tailored to your specific business needs.
           </p>
-        </div>
-        <div className="demo-right">
+        </motion.div>
+        <motion.div
+          className="demo-right"
+          initial="hidden"
+          animate={inView ? 'visible' : 'hidden'}
+          variants={buttonVariants}
+        >
           <button
             className="demo-btn"
             onClick={scrollToRequestDemo}
@@ -37,9 +67,9 @@ const Demo = () => {
           >
             Book a Demo
           </button>
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
